8ef8b173fe987e31fcef68828e7b8c83d619e3f5
Thunderbird560 <howardacael2@gmail.com>
2026-06-23 08:37:32 -0600

    greeting

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Cael Howard 6/23/2026 Lab 7
+Console.WriteLine("Hello user! Welcome to Pig Latin and an offset word! Please input a phrase you want translated to Pig Latin and offsetted.");
+string userPhrase = Console.ReadLine().ToLower();
+
