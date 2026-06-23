e057d2acfdb50fa5f5359f184bcdc456e8189cd8
Thunderbird560 <howardacael2@gmail.com>
2026-06-23 09:01:52 -0600

    functioning pig latin translation

--- Program.cs
+++ Program.cs
@@ -4,5 +4,38 @@ string userPhrase = Console.ReadLine().ToLower();
 
 string[] words = userPhrase.Split(' ');
 
+string pigLatinPhrase = "";
+
+foreach(string word in words)
+{
+    string vowels = "aeiou";
+    int vowelIndex = 0;
+   
+
+        while (vowelIndex < word.Length && !vowels.Contains(word[vowelIndex].ToString()))
+        {
+            vowelIndex++;
+        }
+
+        string pigWord;
+
+        if(vowelIndex == 0)
+        {
+            pigWord = word + "yay";
+        }
+        else if (vowelIndex == word.Length)
+        {
+            pigWord = word + "ay";
+        }
+        else
+        {
+        pigWord = word.Substring(vowelIndex) + word.Substring(0, vowelIndex) + "ay";
+        }
+
+        pigLatinPhrase += pigWord + " ";
+
+    }
+Console.WriteLine("\nPig Latin:");
+Console.WriteLine(pigLatinPhrase);
 
 
--- obj/Debug/net10.0/lab-7-pig-latin-Thunderbird560.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab-7-pig-latin-Thunderbird560.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-7-pig-latin-Thunderbird560")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b3cf5ce35aa602a3106cb71b7f42e04a23c87b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190d61bbc621ef4ff540c9b3f0b21d7877ef4260")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-7-pig-latin-Thunderbird560")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-7-pig-latin-Thunderbird560")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
