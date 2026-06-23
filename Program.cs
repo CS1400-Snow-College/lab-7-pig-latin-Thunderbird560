190d61bbc621ef4ff540c9b3f0b21d7877ef4260
Thunderbird560 <howardacael2@gmail.com>
2026-06-23 08:38:11 -0600

    string and array

--- Program.cs
+++ Program.cs
@@ -2,3 +2,7 @@
 Console.WriteLine("Hello user! Welcome to Pig Latin and an offset word! Please input a phrase you want translated to Pig Latin and offsetted.");
 string userPhrase = Console.ReadLine().ToLower();
 
+string[] words = userPhrase.Split(' ');
+
+
+
